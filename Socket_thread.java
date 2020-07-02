714852528f991f8a2ff8e4eba2516a7c0baa1906
RyoSogabe999 <56621386+RyoSogabe999@users.noreply.github.com>
2020-07-02 22:47:49 +0900

    Update Socket_thread.java

--- Socket_thread.java
+++ Socket_thread.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
+
 public class Socket_thread extends Thread{
     private boolean running = true;
 
@@ -46,66 +47,122 @@ public class Socket_thread extends Thread{
             InputStream is = s.getInputStream();
             ObjectInputStream ois = new ObjectInputStream(is);
 
-            transData data =(transData)ois.readObject();
-            if(data instanceof transData) {
-//                System.out.println(data.get_protocol());
+
+            while(true){
+                // todo break if login success
+                transData data = (transData) ois.readObject();
+
                 int protocol_1 = data.get_protocol();
 
                 // check data is for login?
-                if (protocol_1==10){
+                if (protocol_1 == 10) {
                     // login
-                    if (Server.login(data, os)){
-                        // 認証成功
-                        // ルーム情報送信
-                        transData room_obj = new transData(12);
-                        room_obj.set_room_info(Server.room_info);
-                        os.writeObject(room_obj);
+                    if (Server.login(data, os)) {
+                        // login success
 
                         System.out.println("thread [ " + String.valueOf(num) + " ] : room info sent");
 //                        os.flush();
 
-                        // ユーザネーム設定
+                        // name set
                         this.username = data.get_login_name();
+                        if (num==1){
+                            Server.name_p1 = this.username;
+                        }else if (num==2){
+                            Server.name_p2 = this.username;
+                        }else if (num==3){
+                            Server.name_p3 = this.username;
+                        }else if (num==4){
+                            Server.name_p4 = this.username;
+                        }else if (num==5){
+                            Server.name_p5 = this.username;
+                        }else if (num==6){
+                            Server.name_p6 = this.username;
+                        }else if (num==7){
+                            Server.name_p7 = this.username;
+                        }else if (num==8){
+                            Server.name_p8 = this.username;
+                        }
 
-                    }else{
-                        // 失敗
+
+                        break;
+                    } else {
+                        // login fail
 
                     }
-                }else{
-                    // ログイン用のデータでは無いエラー
+                } else if (protocol_1 == 20) {
+                    // todo register
+                    if(Server.register(data)){
+                        // register success
+                        break;
+                    }else{
+                        // register fail
+
+                    }
+
+
+                } else {
+                    // todo no login and no register data
 
                 }
-            }else{
+
 
             }
 
 
-            //選択ルーム受信
+            // send room info
+            transData room_obj = new transData(12);
+            room_obj.set_room_info(Server.room_info);
+            os.writeObject(room_obj);
+
+
+            //  get selected room
             transData data_room_num =(transData)ois.readObject();
             if (data_room_num.get_protocol()==13){
-                int num = data_room_num.get_room_num();
-                if(Server.room_info.get(num)==0){
-                    // CPU 対戦
-                    Server.room_info.put(num,1);
-                    this.room_num = num;
+                int room_num = data_room_num.get_room_num();
+                if(Server.room_info.get(room_num)==0){
+                    // CPU battle
+                    Server.room_info.put(room_num,1);
+                    this.room_num = room_num;
                     this.running = false;
 
                     int port_send=0;
-                    if(num==1){
+                    if(room_num==1){
                         port_send = 10011;
-                    }else if(num==2){
+                        Server.name_r1_1 = this.username;
+                    }else if(room_num==2){
                         port_send = 10021;
-                    }else if(num==3){
+                        Server.name_r2_1 = this.username;
+                    }else if(room_num==3){
                         port_send = 10031;
-                    }else if(num==4){
+                        Server.name_r3_1 = this.username;
+                    }else if(room_num==4){
                         port_send = 10041;
+                        Server.name_r4_1 = this.username;
                     }
+                    if (num==1){
+                        Server.name_p1 = null;
+                    }else if (num==2){
+                        Server.name_p2 = null;
+                    }else if (num==3){
+                        Server.name_p3 = null;
+                    }else if (num==4){
+                        Server.name_p4 = null;
+                    }else if (num==5){
+                        Server.name_p5 = null;
+                    }else if (num==6){
+                        Server.name_p6 = null;
+                    }else if (num==7){
+                        Server.name_p7 = null;
+                    }else if (num==8){
+                        Server.name_p8 = null;
+                    }
+
                     transData port_send_obj = new transData(85);
                     port_send_obj.set_port_send(port_send);
                     os.writeObject(port_send_obj);
 
                 }else if(Server.room_info.get(num)==1){
-                    // 対局スタート
+                    // battle start
                     Server.room_info.put(num,2);
                     this.room_num = num;
                     this.running = false;
@@ -113,23 +170,50 @@ public class Socket_thread extends Thread{
                     int port_send=0;
                     if(num==1){
                         port_send = 10012;
+                        Server.name_r1_2 = this.username;
                     }else if(num==2){
                         port_send = 10022;
+                        Server.name_r2_2 = this.username;
                     }else if(num==3){
                         port_send = 10032;
+                        Server.name_r3_2 = this.username;
                     }else if(num==4){
                         port_send = 10042;
+                        Server.name_r4_2 = this.username;
                     }
+
+                    if (num==1){
+                        Server.name_p1 = null;
+                    }else if (num==2){
+                        Server.name_p2 = null;
+                    }else if (num==3){
+                        Server.name_p3 = null;
+                    }else if (num==4){
+                        Server.name_p4 = null;
+                    }else if (num==5){
+                        Server.name_p5 = null;
+                    }else if (num==6){
+                        Server.name_p6 = null;
+                    }else if (num==7){
+                        Server.name_p7 = null;
+                    }else if (num==8){
+                        Server.name_p8 = null;
+                    }
+
+
+
                     transData port_send_obj = new transData(85);
                     port_send_obj.set_port_send(port_send);
                     os.writeObject(port_send_obj);
 //                    os.flush();
 
                 }else{
-                    //入れないルームを選択したエラー
-                    //
+                    // false room error
+
 
                 }
+            }else{
+
             }
 
 
@@ -154,9 +238,8 @@ public class Socket_thread extends Thread{
 
 
     public void run() {
-//        while (running){
-//            run_inside();
-//        }
+
+
         run_inside();
 
 
